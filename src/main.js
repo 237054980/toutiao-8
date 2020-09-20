56fc61f36ec6a8a2b4500ecd845779b0bbfee2f0
237054980 <237054980@qq.com>
2020-09-20 14:40:54 +0800

    项目初始化调整

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//加载全局样式
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
+
+//创建根实例  将 router , store 配置到根实例中
+//把 app根组件渲染到 #app 节点
 new Vue({
   router,
   store,
